cd006fc7e75c5b07a5eeee8a11eeb9c0cb4fc248
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-08-20 09:00:20 +0530

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 0,n;
+    printf("Enter range\n");
+    scanf("%d",&n);
+    for(int i = 1 ; i <=n; i++)
+    {
+        if(i%2 == 0) sum = sum + i;
+    }
+    printf("Sum is %d",sum);
+
+    return 0;
+}
